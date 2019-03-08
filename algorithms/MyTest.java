7fb891fd8793b195d68fab1afd3d36f04571e59a
fcb <470429937@qq.com>
2019-03-08 23:55:20 +0800

    add test qaq

--- algorithms/MyTest.java
+++ algorithms/MyTest.java
@@ -1,3 +1,4 @@
+import org.junit.Test;
 import sort.QuickSort;
 
 import java.util.Arrays;
@@ -8,13 +9,18 @@ import java.util.Random;
  * @Date: 2019/3/8
  * @Description: 测试类
  */
-public class Test {
+public class MyTest {
 
     public static void main(String[] args) {
-        int[] arr = new int[30];
+
+    }
+
+    @Test
+    public void quickSortTest(){
+        int[] arr = new int[100];
         Random random = new Random();
-        for (int i = 0; i < 10; i++){
-            arr[i] = random.nextInt(30);
+        for (int i = 0; i < 100; i++){
+            arr[i] = random.nextInt(100);
         }
         System.out.println(Arrays.toString(arr));
         QuickSort sort = new QuickSort();
