df0fad3117e3a5ae4cd9755afbebf8fc2d3dbcb1
FCB <fcb@Excalibur.local>
2020-04-30 23:19:21 +0800

    tmp

--- algorithms/problems/Problem1.java
+++ algorithms/problems/Problem1.java
@@ -5,10 +5,11 @@ package problems;
  * @date 2020/2/23
  * @description
  * 给定一个数组，求如果排序之后，相邻两数的最大差值，要求时间复杂度O(n)，且不能用非基于比较的排序
+ * 思路：假设数组长度是n，将数组分成n+1份，然后遍历。存boolean,max,min，不断用min-max，最后找到最大的
  */
 public class Problem1 {
 
-    public void solution(){
-
+    public int maxGap(int[] arr){
+        return 0;
     }
 }
---
+++ algorithms/problems/Problem2.java
@@ -0,0 +1,12 @@
+package problems;
+
+/**
+ * @author Fcb
+ * @date 2020/2/25
+ * @description 自己实现一个栈，使得一个push(),pop(),getMin()都是O(1)级别的操作
+ * 思路：用两个栈，一个栈存数据，另外一个栈记录当前深度的min
+ */
+public class Problem2 {
+
+
+}
