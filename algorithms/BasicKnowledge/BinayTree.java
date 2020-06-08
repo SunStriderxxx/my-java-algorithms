63805ecc2b805a5f47fcb9aa8f00175dff734b48
FCB <470429937@qq.com>
2020-06-08 23:58:57 +0800

    tmp commit for todo

--- algorithms/BasicKnowledge/BinayTree.java
+++ algorithms/BasicKnowledge/BinayTree.java
@@ -14,34 +14,84 @@ public class BinayTree {
         private int val;
         private TreeNode left;
         private TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode() {
+        }
     }
 
     /**
-     * TODO 递归前序遍历
+     *  递归前序遍历
      */
     public void preOrder(TreeNode node) {
-
+        if (node == null) {
+            return;
+        }
+        System.out.println("val is " + node.val);
+        preOrder(node.left);
+        preOrder(node.right);
     }
 
     /**
-     * TODO 递归中序遍历
+     *  递归中序遍历
      */
     public void inOrder(TreeNode node) {
-
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.println("val is " + node.val);
+        inOrder(node.right);
     }
 
     /**
-     * TODO 递归后序遍历
+     *  TODO 非递归先序遍历
      */
-    public void postOrder(TreeNode node) {
+    public void preOrderNonRecur(TreeNode node) {
 
     }
 
     /**
-     * TODO 判断二叉树是否平衡
+     *  TODO 非递归中序遍历
+     */
+    public void inOrderNonRecur(TreeNode node) {
+
+    }
+
+    /**
+     *  TODO 非递归后序遍历
+     */
+    public void postOrderNonRecur(TreeNode node) {
+
+    }
+
+    /**
+     *  判断二叉树是否平衡
      */
     public boolean isBalance(TreeNode node) {
-        return true;
+        return getLevel(node) != -1;
+    }
+
+    //返回层级，若返回-1代表不平衡
+    private int getLevel(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftLevel = getLevel(node.left);
+        if (leftLevel == -1) {
+            return -1;
+        }
+        int rightLevel = getLevel(node.right);
+        if (rightLevel == -1) {
+            return -1;
+        }
+        if (Math.abs(leftLevel - rightLevel) > 1) {
+            return -1;
+        }
+        return Math.max(leftLevel, rightLevel + 1);
     }
 
 }
