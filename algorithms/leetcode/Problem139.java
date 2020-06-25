d1d5b53b042528ebddc4ede361f87c5f2ccb5a7c
fcb <470429937@qq.com>
2020-06-25 22:05:40 +0800

    20200625

---
+++ algorithms/leetcode/Problem139.java
@@ -0,0 +1,57 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * @author Fcb
+ * @date 2020/6/25
+ * @description 单词拆分
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ *
+ * 说明：
+ *
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * 示例 1：
+ *
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * 示例 2：
+ *
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ *      注意你可以重复使用字典中的单词。
+ * 示例 3：
+ *
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/word-break
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Problem139 {
+
+    /**
+     * 解题思路：动态规划
+     * dp[i]代表String从0到第i-1个位置是否可以组成单词。
+     * 假设0 < j < i
+     * 那么dp[i] = dp[j] + (j-i组成的单词是否在字典中)
+     */
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if(dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
